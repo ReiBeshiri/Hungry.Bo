26e9bda1356d0c837f4f947f93ee3f40638b17ea
Rei Beshiri <rei.beshiri@studio.unibo.it>
2018-12-13 17:27:20 +0100

    bugfix

--- HUNGRY.bo/JS/carrello.js
+++ HUNGRY.bo/JS/carrello.js
@@ -1,11 +1,23 @@
+var total;
 $(document).ready(function() {
+	$( ".prod-qnt" ).bind("change-qnt", function() {
+		var sum = 0;
+	 	$("table>tbody>tr").each(function() {
+			sum += Number($(this).find(".prod-qnt").val()) *
+						Number($(this).find(".prod-price").html());
+	 	});
+	 	sum = sum.toFixed(2);
+	 	total = "Total " + sum;	
+		$("#totalcart").html(total);
+	}).trigger("change-qnt");
 	$( ".prod-qnt" ).change(function() {
 		var sum = 0;
 	 	$("table>tbody>tr").each(function() {
 			sum += Number($(this).find(".prod-qnt").val()) *
 						Number($(this).find(".prod-price").html());
 	 	});
-		console.log(sum);
-	});
-
+	 	sum = sum.toFixed(2);
+	 	total = "Total " + sum;	
+		$("#totalcart").html(total);
+	}).trigger();
 });
