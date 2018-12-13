4c8b44ad75b43d8d570ed18fc2d3a2d6f876e6ee
Mattia Vincenzi <mattia.vincenzi2@studio.unibo.it>
2018-12-13 18:04:46 +0100

    Fixed bugs

--- HUNGRY.bo/JS/carrello.js
+++ HUNGRY.bo/JS/carrello.js
@@ -1,4 +1,3 @@
-var total;
 $(document).ready(function() {
 	$( ".prod-qnt" ).bind("change-qnt", function() {
 		var sum = 0;
@@ -7,17 +6,16 @@ $(document).ready(function() {
 						Number($(this).find(".prod-price").html());
 	 	});
 	 	sum = sum.toFixed(2);
-	 	total = "Total " + sum;	
-		$("#totalcart").html(total);
+		$("#totalcart").html("Total: " + sum);
 	}).trigger("change-qnt");
-	$( ".prod-qnt" ).change(function() {
+
+	$(".prod-qnt").change(function() {
 		var sum = 0;
 	 	$("table>tbody>tr").each(function() {
 			sum += Number($(this).find(".prod-qnt").val()) *
 						Number($(this).find(".prod-price").html());
 	 	});
 	 	sum = sum.toFixed(2);
-	 	total = "Total " + sum;	
-		$("#totalcart").html(total);
+		$("#totalcart").html("Total: " + sum);
 	}).trigger();
 });
