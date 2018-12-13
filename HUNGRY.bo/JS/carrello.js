dbb233a312b7e183a41eb9161408001c3d6c22bd
Mattia Vincenzi <mattia.vincenzi2@studio.unibo.it>
2018-12-13 17:07:19 +0100

    Fixed sum

--- HUNGRY.bo/JS/carrello.js
+++ HUNGRY.bo/JS/carrello.js
@@ -1,30 +1,11 @@
-var total;
 $(document).ready(function() {
-
-	$('input[name^="qnt"]').each(function() {
-    	console.log(($(this).val()));
+	$( ".prod-qnt" ).change(function() {
+		var sum = 0;
+	 	$("table>tbody>tr").each(function() {
+			sum += Number($(this).find(".prod-qnt").val()) *
+						Number($(this).find(".prod-price").html());
+	 	});
+		console.log(sum);
 	});
-
-	var s = "Total " + total;
-
-	$("#totalcart").html(s);
-	
-	var table = document.getElementById('cart');
-
-	var rowLength = table.rows.length;
-
-	for(var i=1; i<rowLength-1; i+=1){
-	  var row = table.rows[i];
-
-	  //your code goes here, looping over every row.
-	  //cells are accessed as easy
-
-	  var cellLength = row.cells.length;
-	  for(var y=0; y<cellLength; y+=1){
-	  	if(y%3===1){
-	  		console.log(row.cells[y].innerHTML);
-	  	}
-	  }
-	}
 
 });
