e7007ebfb392e555df4ba67990f331f8c3f958cc
Mattia Vincenzi <mattia.vincenzi2@studio.unibo.it>
2018-12-13 19:17:05 +0100

    Fixed

--- HUNGRY.bo/JS/carrello.js
+++ HUNGRY.bo/JS/carrello.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-	$( ".prod-qnt" ).bind("change-qnt", function() {
+	$(".prod-qnt").bind("change-qnt", function() {
 		var sum = 0;
 	 	$("table>tbody>tr").each(function() {
 			sum += Number($(this).find(".prod-qnt").val()) *
 						Number($(this).find(".prod-price").html());
 	 	});
 	 	sum = sum.toFixed(2);
-		$("#totalcart").html("Total: " + sum);
+		$("strong.totalcart").html("Total: " + sum);
 	}).trigger("change-qnt");
 
 	$(".prod-qnt").change(function() {
@@ -16,6 +16,17 @@ $(document).ready(function() {
 						Number($(this).find(".prod-price").html());
 	 	});
 	 	sum = sum.toFixed(2);
-		$("#totalcart").html("Total: " + sum);
+		$("strong.totalcart").html("Total: " + sum);
 	}).trigger();
+
+	$(window).bind("resize", function() {
+			if ($(this).width() < 600) {
+					$("tfoot.footer").empty();
+					$("tfoot.footer").html('<tr><td colspan="3" class="align-center"><strong class="totalcart"></strong></td></tr> <tr><td colspan="3" class="text-right pay"><a href="#" class="btn btn-warning">Ordina e paga</a></td></tr>');
+			} else {
+				$("tfoot.footer").empty();
+				$("tfoot.footer").html('<tr><td hidden></td><td class="align-center"><strong class="totalcart"></strong></td> <td class="text-right pay"><a href="#" class="btn btn-warning">Ordina e paga</a></td></tr>');
+			}
+	}).trigger('resize');
+
 });
