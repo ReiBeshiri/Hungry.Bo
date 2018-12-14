3c2bc28c9d468306666134cbacd7d475896b9751
Mattia Vincenzi <mattia.vincenzi2@studio.unibo.it>
2018-12-14 12:28:57 +0100

    Fixed table

--- HUNGRY.bo/JS/client_visiting_restaurant.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$(window).bind("resize", function () {
-      if ($(this).width() < 981) {
-          $("#breakdiv").addClass("w-100");
-      } else {
-        $("#breakdiv").removeClass("w-100");
-      }
-  }).trigger('resize');
-});
