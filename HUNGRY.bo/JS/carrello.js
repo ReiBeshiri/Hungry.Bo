04414f5cef6411489f51fe8197c1fa663a8fc156
Mattia Vincenzi <mattia.vincenzi2@studio.unibo.it>
2018-12-14 11:50:04 +0100

    fixed cart

--- HUNGRY.bo/JS/carrello.js
+++ HUNGRY.bo/JS/carrello.js
@@ -1,32 +1,43 @@
 $(document).ready(function() {
-	$(".prod-qnt").bind("change-qnt", function() {
-		var sum = 0;
-	 	$("table>tbody>tr").each(function() {
-			sum += Number($(this).find(".prod-qnt").val()) *
-						Number($(this).find(".prod-price").html());
-	 	});
-	 	sum = sum.toFixed(2);
-		$("strong.totalcart").html("Total: " + sum);
-	}).trigger("change-qnt");
+
+	computeInitPrice();
 
 	$(".prod-qnt").change(function() {
-		var sum = 0;
-	 	$("table>tbody>tr").each(function() {
-			sum += Number($(this).find(".prod-qnt").val()) *
-						Number($(this).find(".prod-price").html());
-	 	});
-	 	sum = sum.toFixed(2);
-		$("strong.totalcart").html("Total: " + sum);
-	}).trigger();
+		computeDinamicallyPrice($(this));
+	});
 
 	$(window).bind("resize", function() {
 			if ($(this).width() < 600) {
 					$("tfoot.footer").empty();
-					$("tfoot.footer").html('<tr><td colspan="3" class="align-center"><strong class="totalcart"></strong></td></tr> <tr><td colspan="3" class="text-right pay"><a href="#" class="btn btn-warning">Ordina e paga</a></td></tr>');
+					$("tfoot.footer").html('<tr><td colspan="3" class="text-center"><strong class="totalcart"></strong></td></tr> <tr><td colspan="3" class="text-center pay"><a href="#" class="btn btn-warning">Ordina e paga</a></td></tr>');
 			} else {
 				$("tfoot.footer").empty();
-				$("tfoot.footer").html('<tr><td hidden></td><td class="align-center"><strong class="totalcart"></strong></td> <td class="text-right pay"><a href="#" class="btn btn-warning">Ordina e paga</a></td></tr>');
+				$("tfoot.footer").html('<tr><td class="dummy-column"></td><td class="align-center"><strong class="totalcart"></strong></td><td class="text-center"><a href="#" class="btn btn-warning">Ordina e paga</a></td></tr>');
 			}
+			computeInitPrice();
 	}).trigger('resize');
 
 });
+
+function computeDinamicallyPrice(ref) {
+	var sum = 0;
+	$(ref.parents("table").children("tbody").children("tr")).each(function() {
+		sum += Number($(this).find(".prod-qnt").val()) *
+					Number($(this).find(".prod-price").html());
+	});
+	sum = sum.toFixed(2);
+	$(ref.parents("table").find("strong.totalcart")).html("Total: " + sum);
+}
+
+function computeInitPrice() {
+	var tables = $("table");
+	tables.each(function(){
+		var sum = 0;
+		$($(this).children("tbody").children("tr")).each(function() {
+			sum += Number($(this).find(".prod-qnt").val()) *
+						Number($(this).find(".prod-price").html());
+		});
+		sum = sum.toFixed(2);
+		$($(this).find("strong.totalcart")).html("Total: " + sum);
+	});
+}
