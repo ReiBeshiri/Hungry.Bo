14c0b139ccdaedf80c85df90a437b66b46ff1c7b
Rei Beshiri <rei.beshiri@studio.unibo.it>
2018-12-14 15:01:04 +0100

    fiix

---
+++ HUNGRY.bo/JS/supplier_manage_orders.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(window).bind("resize", function () {
+      if ($(this).width() < 981) {
+          $("td.order-state").attr("hidden", "true");
+      	  $("th.order-prod").attr("hidden", "true");
+      } else {
+      	  $("td.order-state").removeAttr("hidden");
+		  $("th.order-prod").removeAttr("hidden");
+      }
+  }).trigger('resize');
+});
