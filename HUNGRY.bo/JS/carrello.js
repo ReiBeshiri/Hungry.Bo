0d874a6cb519b70e0916097ba47a184aba835146
Mattia Vincenzi <mattia.vincenzi2@studio.unibo.it>
2018-12-15 18:04:04 +0100

    Added ordina & paa

--- HUNGRY.bo/JS/carrello.js
+++ HUNGRY.bo/JS/carrello.js
@@ -9,10 +9,10 @@ $(document).ready(function() {
 	$(window).bind("resize", function() {
 			if ($(this).width() < 600) {
 					$("tfoot.footer").empty();
-					$("tfoot.footer").html('<tr><td colspan="4" class="text-center"><strong class="totalcart"></strong></td></tr> <tr><td colspan="4" class="text-center pay"><a href="#" class="btn btn-warning">Ordina e paga</a></td></tr>');
+					$("tfoot.footer").html('<tr><td colspan="4" class="text-center"><strong class="totalcart"></strong></td></tr> <tr><td colspan="4" class="text-center pay"><a href="#" data-toggle="modal" data-target="#order-pay" class="btn btn-warning">Ordina e paga</a></td></tr>');
 			} else {
 				$("tfoot.footer").empty();
-				$("tfoot.footer").html('<tr><td class="dummy-column"></td><td class="align-center"><strong class="totalcart"></strong></td><td class="text-center"><a href="#" class="btn btn-warning">Ordina e paga</a></td></tr>');
+				$("tfoot.footer").html('<tr><td class="dummy-column"></td><td class="align-center"><strong class="totalcart"></strong></td><td class="text-center"><a href="#" data-toggle="modal" data-target="#order-pay" class="btn btn-warning">Ordina e paga</a></td></tr>');
 			}
 			computeInitPrice();
 	}).trigger('resize');
