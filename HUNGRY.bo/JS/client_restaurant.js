ef4867d0d5e1809a7e44383bafca49ef84a2b153
Mattia Vincenzi <mattia.vincenzi2@studio.unibo.it>
2018-12-18 23:00:57 +0100

    Added links to cart

--- HUNGRY.bo/JS/client_restaurant.js
+++ HUNGRY.bo/JS/client_restaurant.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
 
   $(window).bind("resize", function () {
       if ($(this).width() < 981) {
-          $("ul.btn-cart").empty();
-          $("ul.btn-cart").html('<a href="#"> <img src="../res/cart.png" width="60" height="60" alt=""/> </a>');
+          $("ul.btn-cart>li").empty();
+          $("ul.btn-cart>li").html('<a href="./carrello.html"> <img src="../res/cart.png" width="60" height="60" alt=""/> </a>');
           $("tr>td>span.ingredients-in-table").attr("hidden", "true");
       } else {
-        $("ul.btn-cart").empty();
-        $("ul.btn-cart").html('<button id="carrello" type="button" class="btn btn-outline-info">Carrello</button>');
+        $("ul.btn-cart>li").empty();
+        $("ul.btn-cart>li").html('<a href="./carrello.html"> <button id="cart" type="button" class="btn btn-outline-info">Carrello</button> </a>');
         $("tr>td>span.ingredients-in-table").removeAttr("hidden");
       }
   }).trigger('resize');
