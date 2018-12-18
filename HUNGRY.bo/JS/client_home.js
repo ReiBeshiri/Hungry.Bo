421f3a7008fd8a759f1fb8336bbcaf09fae2dbfd
Mattia Vincenzi <mattia.vincenzi2@studio.unibo.it>
2018-12-18 22:54:52 +0100

    Added cart ref

--- HUNGRY.bo/JS/client_home.js
+++ HUNGRY.bo/JS/client_home.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
   $(window).bind("resize", function () {
       if ($(this).width() < 981) {
-          $("ul.btn-cart").empty();
-          $("ul.btn-cart").html('<a href="#"> <img src="../res/cart.png" width="60" height="60" alt=""/> </a>');
+          $("ul.btn-cart>li").empty();
+          $("ul.btn-cart>li").html('<a href="./carrello.html"> <img src="../res/cart.png" width="60" height="60" alt=""/> </a>');
       } else {
-        $("ul.btn-cart").empty();
-        $("ul.btn-cart").html('<button id="carrello" type="button" class="btn btn-outline-info">Carrello</button>');
+        $("ul.btn-cart>li").empty();
+        $("ul.btn-cart>li").html('<a href="./carrello.html"><button id="cart" type="button" class="btn btn-outline-info">Carrello</button></a>');
       }
   }).trigger('resize');
 });
