4bf6e0ea6577c0ff0122daec6d00151d2c151ca9
Rei Beshiri <rei.beshiri@studio.unibo.it>
2018-12-20 19:41:38 +0100

    popup admin

--- HUNGRY.bo/JS/Admin.js
+++ HUNGRY.bo/JS/Admin.js
@@ -5,17 +5,17 @@ $(window).bind("resize", function () {
         $("th#nome-locale").hide();
         $("td[headers='nome-locale']").hide();
         $("td[headers='modify']>span").empty();
-        $("td[headers='modify']>span").html('<a href="#" data-toggle="modal" data-target="#modify-popup"><img width="30px" heigth="30px" src="../res/modify-icon.png" alt="modify"/></a>');
+        $("td[headers='modify']>span").html('<a href="#"><img width="30px" heigth="30px" src="../res/modify-icon.png" alt="modify" data-toggle="modal" data-target="#modify-from-admin"/></a>');
         $("td[headers='notify']>span").empty();
-        $("td[headers='notify']>span").html('<a href="#" data-toggle="modal" data-target="#notify-popup"><img width="30px" heigth="30px" src="../res/notify-icon.png" alt="notify"/></a>');
+        $("td[headers='notify']>span").html('<a href="#"><img width="30px" heigth="30px" src="../res/notify-icon.png" alt="notify" data-toggle="modal" data-target="#send-notify-from-admin"/></a>');
     } else {
       $("th#password").show();
       $("td[headers='password']").show();
       $("th#nome-locale").show();
       $("td[headers='nome-locale']").show();
       $("td[headers='modify']>span").empty();
-      $("td[headers='modify']>span").html('<button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#modify-popup">Modifica</button>');
+      $("td[headers='modify']>span").html('<button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#modify-from-admin">Modifica</button>');
       $("td[headers='notify']>span").empty();
-      $("td[headers='notify']>span").html('<button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="">Notifica</button>');
+      $("td[headers='notify']>span").html('<button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#send-notify-from-admin">Notifica</button>');
     }
 }).trigger('resize');
