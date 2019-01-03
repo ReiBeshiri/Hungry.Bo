6ab9d625d14e03e3f7df1dcf4c64dd3dabaa612c
Rei Beshiri <rei.beshiri@studio.unibo.it>
2019-01-03 20:00:08 +0100

    add dinamic  card

--- HUNGRY.bo/JS/client_home.js
+++ HUNGRY.bo/JS/client_home.js
@@ -1,4 +1,6 @@
 var nomeLocale = "";
+var suppliers;
+var nomilocali;
 var voti = 0;
 $(document).ready(function () {
   $(window).bind("resize", function () {
@@ -10,6 +12,25 @@ $(document).ready(function () {
         $("ul.btn-cart>li").html('<a href="./carrello.html"><button id="cart" type="button" class="btn btn-outline-info">Carrello</button></a>');
       }
   }).trigger('resize');
+
+  $.getJSON("../PHP/suppliers.php?request=suppliers", function(data) {
+
+    if(data.status === "error") {
+
+      console.log("error");
+
+    } else{
+
+      suppliers = data;
+      nomilocali = suppliers.status[0]["NomeLocale"];
+      console.log(nomilocali);
+
+    }
+
+    $("#greed div").append('<div class="col-lg-4 col-md-6 mb-4 col-xl-3"><div class="card"><div class="view overlay hm-white-slight"><a href="#"><img class="img-fluid local-image" src="../res/pizzamargherita.jpg" alt="local imgage"/><img class="img-fluid rounded-circle icon float-left ml-3" src="../res/icona.png" alt="local icon"/><div class="card-body"><h6 class="card-title text-center">'+nomilocali+'</h6><p class="card-text text-muted text-center vote">Voto: <span class="avg-score"></span></p></div></a><div class="card-footer text-right"><small class="card-text text-muted comment"><a href="#" data-toggle="modal" data-target="#rec-popup">Scrivi una recensione</a></small></div></div></div></div>');
+
+  });
+
 
   $.getJSON("../PHP/scores.php?request=voti", function(data) {
 
@@ -26,6 +47,7 @@ $(document).ready(function () {
       }
 
   });
+
 
   //get nomelocale when click on "scrivi reecensione"
   $("small a").click(function() {
---
+++ HUNGRY.bo/PHP/suppliers.php
@@ -0,0 +1,55 @@
+<?php
+  include('functions.php');
+  header('Content-Type: application/json');
+  define("HOST", "localhost"); // E' il server a cui ti vuoi connettere.
+  define("USER", "root"); // E' l'utente con cui ti collegherai al DB.
+  define("PASSWORD", ""); // Password di accesso al DB.
+  define("DATABASE", "HUNGRYbo"); // Nome del database.
+
+  //session
+  sec_session_start();
+
+  if(isset($_GET["request"])) {
+
+  //start conn
+  $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+
+  // Check connection
+  if ($mysqli->connect_error) {
+
+      $response_array['status'] = "Errore: Connessione con il DB non riuscita";
+      echo json_encode($response_array);
+      die();
+
+  }
+
+    //get Fornitori
+    $stmt = $mysqli->prepare("SELECT NomeLocale FROM Fornitore");
+
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    $suppliers = array();
+    $nameSupp = array();
+
+    while($row = $result->fetch_assoc()){
+      $suppliers[] = $row;
+    }
+
+    $stmt->close();
+
+    if($suppliers == NULL){
+      $suppliers = "-";
+    }
+
+    $response_array['status'] = $suppliers;
+    print json_encode($response_array);
+    die();
+
+  } else {
+    $response_array['status'] = "error";
+    print json_encode($response_array);
+    die();
+  }
+ ?>
