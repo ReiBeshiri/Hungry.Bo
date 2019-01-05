77dbf4bbf66178cf7bac876c8f7baf8e2fbd06b7
Mattia Vincenzi <mattia.vincenzi2@studio.unibo.it>
2019-01-05 14:07:31 +0100

    Basic orders done

--- HUNGRY.bo/JS/supplier_orders.js
+++ HUNGRY.bo/JS/supplier_orders.js
@@ -3,18 +3,25 @@ $(document).ready(function () {
   $('#desc-other').attr("hidden", "true");
 
   $(window).bind("resize", function () {
-      if ($(this).width() < 981) {
-          $("td.order-state").attr("hidden", "true");
-      	  $("th.order-prod").attr("hidden", "true");
+      if ($(this).width() < 680) {
+        $("td.order-state").attr("hidden", "true");
+      	$("th#prod").attr("hidden", "true");
+        $("td.luogo").attr("hidden", "true");
+      	$("th#place").attr("hidden", "true");
+      } else if ($(this).width() < 980) {
+        $("th#place").removeAttr("hidden");
+        $("td.luogo").removeAttr("hidden");
       } else {
-      	  $("td.order-state").removeAttr("hidden");
-		      $("th.order-prod").removeAttr("hidden");
+      	$("td.order-state").removeAttr("hidden");
+		    $("th#prod").removeAttr("hidden");
+        $("th#place").removeAttr("hidden");
+        $("td.luogo").removeAttr("hidden");
       }
   }).trigger('resize');
 
 
   $('#order-status-select').change(function() {
-  	if($('#order-status-select').val() == 3){
+  	if($('#order-status-select').val() == "Altro..."){
   		$('#desc-other').removeAttr("hidden");
   	}else{
   		$('#desc-other').attr("hidden", "true");
@@ -22,16 +29,71 @@ $(document).ready(function () {
   });
 
   $.post("../PHP/supplier_orders.php?request=orders", function(orders) {
-    console.log(orders);
-
     var html_code = "";
 
     //Creazine della tabella.
     for(var i = 0; i < orders.length; i++) {
-      html_code += '<tr><td headers="id" hidden>'+orders[i].ID+'</td><td headers="dest">'+orders[i].UsernameCliente+'</td><td headers="prod" class="order-state">'+orders[i].Stato+'</td><td headers="place">'+orders[i].LuogoConsegna+'</td><td headers="hour">'+(orders[i].Ora).slice(0,5)+'</td><td headers="notify"><a href="#" data-toggle="modal" data-target="#order-manage">Dettagli</a></td></tr>';
+      html_code += '<tr><td headers="id" class="id" hidden>'+orders[i].ID+'</td><td headers="dest" class="cliente">'+orders[i].UsernameCliente+'</td><td headers="prod" class="order-state">'+orders[i].Stato+'</td><td headers="place" class="luogo">'+orders[i].LuogoConsegna+'</td><td headers="hour" class="ora">'+(orders[i].Ora).slice(0,5)+'</td><td headers="notify"><a class="dettagli" href="#" data-toggle="modal" data-target="#order-manage">Dettagli</a></td></tr>';
     }
 
     $("table>tbody").html(html_code);
+    window.parent.$(window.parent.document).trigger('resize');
   });
 
+  $("tbody").on('click', 'a.dettagli', function(){
+    var id = $(this).parents("tr").children("td.id").text();
+    var luogo = $(this).parents("tr").children("td.luogo").text();
+    var ora = $(this).parents("tr").children("td.ora").text();
+    var cliente = $(this).parents("tr").children("td.cliente").text();
+    var stato = $(this).parents("tr").children("td.order-state").text();
+
+    var dataToSend = {
+      id: id
+    };
+
+    $.post("../PHP/supplier_orders.php?request=details", dataToSend, function(pInOrder) {
+      $("span#id-sel-order").text(id);
+      $("span#cliente").text(cliente);
+      $("span#luogo").text(luogo);
+      $("span#ora").text(ora);
+      $("select#order-status-select option[value="+stato+"]").attr('selected', 'selected');
+      var html_code = "<ul>";
+      for(var i = 0; i < pInOrder.length; i++) {
+        var idProdotto = pInOrder[i].IDProdotto;
+
+        var dataToSend = {
+          id: idProdotto
+        }
+        $.ajax({
+            url: "../PHP/supplier_orders.php?request=product-name",
+            type: "POST",
+            async: false,
+            dataType: "json",
+            data: dataToSend,
+            success: function(name) {
+              if(name.status == 'success') {
+                html_code += '<li>'+name.nome+' <span class="qnta">'+pInOrder[i].qnta+'</span><br/>('+pInOrder[i].Descrizione+')</li>';
+              }
+        }});
+      }
+      html_code += "</ul>";
+      $("span#prodottiInOrdine").html(html_code);
+    });
+  });
+
+  $("div#order-manage button").click(function() {
+    var id = $("span#id-sel-order").text();
+    var stato = $("#order-status-select").val();
+
+    var data = {
+      id: id,
+      stato: stato
+    }
+
+    $.post("../PHP/supplier_orders.php?request=update-status", data, function(dataRecv) {
+      if(dataRecv.status == "success") {
+        location.reload();
+      }
+    });
+  });
 });
--- HUNGRY.bo/PHP/supplier_orders.php
+++ HUNGRY.bo/PHP/supplier_orders.php
@@ -41,6 +41,89 @@ if(isset($_GET['request'])) {
       print json_encode($output);
 
       break;
+
+    case 'details':
+      if(isset($_POST['id'])) {
+        $stmt = $mysqli->prepare("SELECT * FROM ProdottoInOrdine WHERE IDOrdine = ?");
+
+        if($stmt == false) {
+          $response_array['status'] = "Errore nella query";
+          print json_encode($response_array);
+          die();
+        }
+
+        $stmt->bind_param('i', $_POST['id']);
+
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        $output = array();
+        while($row = $result->fetch_assoc()){
+            $output[] = $row;
+        }
+        $stmt->close();
+
+        print json_encode($output);
+      } else {
+        $response_array['status'] = "ID mancante";
+        print json_encode($response_array);
+      }
+      break;
+
+    case 'product-name':
+      if(isset($_POST['id'])) {
+        $stmt = $mysqli->prepare("SELECT Nome FROM Prodotto WHERE ID = ?");
+
+        if($stmt == false) {
+          $response_array['status'] = "Errore nella query";
+          print json_encode($response_array);
+          die();
+        }
+
+        $stmt->bind_param('i', $_POST['id']);
+
+        $stmt->execute();
+
+        $stmt->bind_result($nome);
+
+        $stmt->fetch();
+
+        $response_array['status'] = 'success';
+
+        $response_array['nome'] = $nome;
+
+        print json_encode($response_array);
+      } else {
+        $response_array['status'] = "ID mancante";
+        print json_encode($response_array);
+      }
+      break;
+
+    case 'update-status':
+      if(isset($_POST['stato']) && isset($_POST['id'])) {
+        $stmt = $mysqli->prepare("UPDATE Ordine SET Stato=? WHERE ID=?");
+
+        if($stmt == false) {
+          $response_array['status'] = "Errore nella query";
+          print json_encode($response_array);
+          die();
+        }
+
+        $stmt->bind_param('si', $_POST['stato'], $_POST['id']);
+
+        $stmt->execute();
+
+        $stmt->close();
+
+        $response_array['status'] = 'success';
+
+        print json_encode($response_array);
+      } else {
+        $response_array['status'] = "ID e/o stato mancante/i";
+        print json_encode($response_array);
+      }
+      break;
   }
 
   $mysqli->close();
