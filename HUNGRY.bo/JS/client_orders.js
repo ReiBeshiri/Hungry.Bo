d11f7f37013e8793eb8982df6830449ca271d06f
Rei Beshiri <rei.beshiri@studio.unibo.it>
2019-01-07 01:37:37 +0100

    fix

--- HUNGRY.bo/JS/client_orders.js
+++ HUNGRY.bo/JS/client_orders.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
         }
     }).trigger('resize');
 
+
     $.getJSON("../PHP/client_orders.php?request=orders", function(data) {
 
       if(data != null) {
@@ -39,9 +40,9 @@ $(document).ready(function(){
             }
           }
         }
-        console.log(arrayOrd);
-        console.log(arrayDet);
-        console.log(deliveryData);
+        ///console.log(arrayOrd);
+        //console.log(arrayDet);
+        //console.log(deliveryData);
 
         for(var i = 0; i < ordersData.length; i++){
             //ora Consegna
@@ -58,10 +59,74 @@ $(document).ready(function(){
                 }
               }
           }
-            $("#tbody").append('<tr><td headers="id" hidden>'+ordersData[i]["ID"]+'</td><td headers="dest">'+ordersData[i]["UsernameCliente"]+'</td><td headers="prod" class="order-state">'+ordersData[i]["Stato"]+'</td><td headers="place">'+ordersData[i]["LuogoConsegna"]+'</td><td headers="hour">'+maxTime+'</td><td headers="details"><a href="#" data-toggle="modal" data-target="#order-details">Dettagli</a></td></tr>');
+            $("#tbody").append('<tr><td headers="id" hidden class="hiddentd">'+ordersData[i]["ID"]+'</td><td headers="dest">'+ordersData[i]["UsernameCliente"]+'</td><td headers="prod" class="order-state">'+ordersData[i]["Stato"]+'</td><td headers="place">'+ordersData[i]["LuogoConsegna"]+'</td><td headers="hour">'+maxTime+'</td><td headers="details"><a href="#" data-toggle="modal" data-target="#order-details" class="details">Dettagli</a></td></tr>');
+        }
+    } else {
+      console.log("An error in the Server as occurred");
+    }
+    });
+
+    /*<div class="form-group">
+      <p>Mittente: <span>Chianì</span></p>
+    </div>
+    <div class="form-group">
+      Ordine:
+      <ul>
+        <li>margherita <span>(Modifiche)</span></li>
+        <li>diavola <span>(Modifiche)</span></li>
+        <li>5 stagioni <span>(Modifiche)</span></li>
+      </ul>
+    </div>
+    <div class="form-group">
+      <p>Stato: <span>Non ancora spedito</span></p>
+    </div>
+    <div class="form-group">
+      <p>Luogo: <span>aula 2.2</span></p>
+    </div>
+    <div class="form-group">
+      <p>Ora: <span>12:30</span></p>
+    </div>*/
+    $("#tbody").on('click', 'a.details', function(){  //attacco dinamicamente a tbody l'onclick agli ancor class details
+
+      idOrder = $(this).parents("tr").children("td.hiddentd").text();
+      spedizione = $(this).parents("tr").children("td.hiddentd").next().next().text();
+      luogoConsegna = $(this).parents("tr").children("td.hiddentd").next().next().next().text();
+      oraConsegna = $(this).parents("tr").children("td.hiddentd").next().next().next().next().text();
+
+      //prendo l'id dell'oridine
+      var dataToSend = {
+        id: idOrder
+      };
+
+      $.post("../PHP/client_orders.php?request=details", dataToSend, function(data) {
+
+        if(data != null) {
+
+          var output = data;
+          var str2 = '';
+          var str1 = '<div class="form-group"><p>Mittente: <span>'+output[0]+'</span></p></div><div class="form-group">Ordine:<ul>';
+
+          var str3 = '</ul></div><div class="form-group"><p>Stato: <span>'+spedizione+'</span></p></div><div class="form-group"><p>Luogo: <span>'+luogoConsegna+'</span></p></div><div class="form-group"><p>Ora: <span>'+oraConsegna+'</span></p></div>';
+
+          for (var i = 1; i < output.length; i++) {
+            if(output[i]["Descrizione"] === null){
+              output[i]["Descrizione"] = "";
+            }
+            str2 += '<li>'+output[i][0] +'<span>'+'  '+output[i]["Descrizione"]+'</span></li>';
+          }
+
+          var str = str1+str2+str3;
+
+          $("div.modal-body").empty();
+          $("div.modal-body").append(str);
+
+        } else {
+
+          console.log("An error in the Server as occurred");
+
         }
 
-    }
+      });
     });
 
 });
--- HUNGRY.bo/PHP/client_orders.php
+++ HUNGRY.bo/PHP/client_orders.php
@@ -17,153 +17,231 @@
 
       $usernameClient = $_SESSION['username'];
 
-      $stmt = $mysqli->prepare("SELECT * FROM Ordine WHERE UsernameCliente = ?");
+      switch ($_GET["request"]) {
 
-      $stmt->bind_param('s', $usernameClient);
+        case 'orders':
 
-      $stmt->execute();
+          $stmt = $mysqli->prepare("SELECT * FROM Ordine WHERE UsernameCliente = ?");
 
-      $result = $stmt->get_result();
-
-      $ordersData = array();
-      while($row = $result->fetch_assoc()){
-          $ordersData[] = $row;
-      }
-      $stmt->close();
-
-      /*
-      array(1) {
-        [0]=>
-        array(5) {
-          ["ID"]=>
-          int(1)
-          ["Stato"]=>
-          string(18) "Non ancora spedito"
-          ["UsernameCliente"]=>
-          string(6) "mattia"
-          ["LuogoConsegna"]=>
-          string(5) "atrio"
-          ["UsernameFornitore"]=>
-          string(9) "hamburger"
-        }
-      }
-      */
-
-      //var_dump($ordersData[i]["CAMPO"]);
-
-      //unset $result per evitare problemi
-      $productsInOrdersData = array();
-      unset($result);
-
-      if(count($ordersData) > 0){
-        for ($i = 0; $i < count($ordersData); $i++) {
-
-          $stmt = $mysqli->prepare("SELECT * FROM ProdottoInOrdine WHERE IDOrdine = ?");
-
-          $stmt->bind_param('i', $ordersData[$i]["ID"]);
+          $stmt->bind_param('s', $usernameClient);
 
           $stmt->execute();
 
           $result = $stmt->get_result();
 
+          $ordersData = array();
+          while($row = $result->fetch_assoc()){
+              $ordersData[] = $row;
+          }
+          $stmt->close();
+
+          /*
+          array(1) {
+            [0]=>
+            array(5) {
+              ["ID"]=>
+              int(1)
+              ["Stato"]=>
+              string(18) "Non ancora spedito"
+              ["UsernameCliente"]=>
+              string(6) "mattia"
+              ["LuogoConsegna"]=>
+              string(5) "atrio"
+              ["UsernameFornitore"]=>
+              string(9) "hamburger"
+            }
+          }
+          */
+
+          //var_dump($ordersData[i]["CAMPO"]);
+
+          //unset $result per evitare problemi
+          $productsInOrdersData = array();
+          unset($result);
+
+          if(count($ordersData) > 0){
+            for ($i = 0; $i < count($ordersData); $i++) {
+
+              $stmt = $mysqli->prepare("SELECT * FROM ProdottoInOrdine WHERE IDOrdine = ?");
+
+              $stmt->bind_param('i', $ordersData[$i]["ID"]);
+
+              $stmt->execute();
+
+              $result = $stmt->get_result();
+
+              while($row = $result->fetch_assoc()){
+                  $productsInOrdersData[] = $row;
+              }
+              $stmt->close();
+            }
+
+            //var_dump($productInOrdersData[0]["CAMPO"]);  $productInOrdersData[0]["IDProdotto"];
+            /*
+            array(1) {
+              [i]=>
+              array(5) {
+                ["IDProdotto"]=>
+                int(35)
+                ["IDOrdine"]=>
+                int(1)
+                ["ID"]=>
+                int(3)
+                ["qnta"]=>
+                int(1)
+                ["Descrizione"]=>
+                string(17) "Pizza senza farro"
+              }
+            }
+            */
+          }
+
+          $productsData = array();
+          unset($result);
+          if(count($productsInOrdersData) > 0){
+            for ($i = 0; $i < count($productsInOrdersData); $i++) {
+
+              $stmt = $mysqli->prepare("SELECT * FROM Prodotto WHERE ID = ?");
+
+              $stmt->bind_param('i', $productsInOrdersData[$i]["IDProdotto"]);
+
+              $stmt->execute();
+
+              $result = $stmt->get_result();
+
+              while($row = $result->fetch_assoc()){
+                  $productsData[] = $row;
+              }
+              $stmt->close();
+            }
+            /*
+            array(1) {
+              [i]=>
+              array(7) {
+                ["ID"]=>
+                int(35)
+                ["Nome"]=>
+                string(5) "pizza"
+                ["Prezzo"]=>
+                int(10)
+                ["TempoPreparazione"]=>
+                int(15)
+                ["Ingredienti"]=>
+                string(20) "pomodoro, mozzarella"
+                ["TipoProdotto"]=>
+                string(12) "primo piatto"
+                ["UsernameFornitore"]=>
+                string(9) "hamburger"
+              }
+            }
+            */
+          }
+
+          $deliveryData = array();
+          unset($result);
+          if(count($ordersData) > 0){
+            for ($i = 0; $i < count($ordersData); $i++) {
+
+              $stmt = $mysqli->prepare("SELECT Username, TempoArrivoCampus FROM Fornitore WHERE Username = ?");
+
+              $stmt->bind_param('s', $ordersData[$i]["UsernameFornitore"]);
+
+              $stmt->execute();
+
+              $result = $stmt->get_result();
+
+              while($row = $result->fetch_assoc()){
+                  $deliveryData[] = $row;
+              }
+              $stmt->close();
+            }
+          }
+
+          //var_dump($ordersData);
+          //var_dump($productsInOrdersData);
+          //var_dump($productsData);
+          //var_dump($deliveryData);
+
+          $response = array();
+          array_push($response, $ordersData, $productsInOrdersData, $productsData, $deliveryData);
+
+          //$response_array['status'] = "success";
+          print json_encode($response);
+
+        break;//case orders
+
+
+        case 'details':
+
+          if(!isset($_POST["id"])) {die();}
+
+          $idordine = (int) $_POST["id"];
+          $response_array = array();
+
+          $stmt = $mysqli->prepare("SELECT UsernameFornitore FROM Ordine WHERE ID = ?");
+
+          $stmt->bind_param('i', $idordine);
+
+          $stmt->execute();
+
+          $result = $stmt->get_result();
+
+          $ordersData = array();
+          while($row = $result->fetch_assoc()){
+              $ordersData[] = $row;
+          }
+          $stmt->close();
+
+          array_push($response_array, $ordersData[0]["UsernameFornitore"]);
+
+          //seconda query
+          unset($result);
+
+          $stmt = $mysqli->prepare("SELECT IDProdotto,Qnta,Descrizione FROM ProdottoInOrdine WHERE IDOrdine = ?");
+
+          $stmt->bind_param('i', $idordine);
+
+          $stmt->execute();
+
+          $result = $stmt->get_result();
+
+          $productsInOrdersData = array();
           while($row = $result->fetch_assoc()){
               $productsInOrdersData[] = $row;
           }
           $stmt->close();
-        }
 
-        //var_dump($productInOrdersData[0]["CAMPO"]);  $productInOrdersData[0]["IDProdotto"];
-        /*
-        array(1) {
-          [i]=>
-          array(5) {
-            ["IDProdotto"]=>
-            int(35)
-            ["IDOrdine"]=>
-            int(1)
-            ["ID"]=>
-            int(3)
-            ["qnta"]=>
-            int(1)
-            ["Descrizione"]=>
-            string(17) "Pizza senza farro"
+          $productsData = array();
+          unset($result);
+
+          for ($i=0; $i < count($productsInOrdersData); $i++) {
+            $stmt = $mysqli->prepare("SELECT Nome FROM Prodotto WHERE ID = ?");
+
+            $stmt->bind_param('i', $productsInOrdersData[$i]["IDProdotto"]);
+
+            $stmt->execute();
+
+            $result = $stmt->get_result();
+
+            while($row = $result->fetch_assoc()){
+                $productsData[] = $row;
+            }
+            $stmt->close();
+
+            array_push($productsInOrdersData[$i], $productsData[$i]["Nome"]);
+            array_push($response_array, $productsInOrdersData[$i]);
+
           }
-        }
-        */
-      }
 
-      $productsData = array();
-      unset($result);
-      if(count($productsInOrdersData) > 0){
-        for ($i = 0; $i < count($productsInOrdersData); $i++) {
+          //var_dump($response_array);
+          //[0] -> UsernameFornitore [1-array.length]->prodotti nell'Ordine dove [i]["Qnta"] [i]["Descrizione"] [i][0]->nome pietanza
 
-          $stmt = $mysqli->prepare("SELECT * FROM Prodotto WHERE ID = ?");
+          //$response_array['status'] = "success";
+          print json_encode($response_array);
 
-          $stmt->bind_param('i', $productsInOrdersData[$i]["IDProdotto"]);
+        break;
 
-          $stmt->execute();
-
-          $result = $stmt->get_result();
-
-          while($row = $result->fetch_assoc()){
-              $productsData[] = $row;
-          }
-          $stmt->close();
-        }
-        /*
-        array(1) {
-          [i]=>
-          array(7) {
-            ["ID"]=>
-            int(35)
-            ["Nome"]=>
-            string(5) "pizza"
-            ["Prezzo"]=>
-            int(10)
-            ["TempoPreparazione"]=>
-            int(15)
-            ["Ingredienti"]=>
-            string(20) "pomodoro, mozzarella"
-            ["TipoProdotto"]=>
-            string(12) "primo piatto"
-            ["UsernameFornitore"]=>
-            string(9) "hamburger"
-          }
-        }
-        */
-      }
-
-      $deliveryData = array();
-      unset($result);
-      if(count($ordersData) > 0){
-        for ($i = 0; $i < count($ordersData); $i++) {
-
-          $stmt = $mysqli->prepare("SELECT Username, TempoArrivoCampus FROM Fornitore WHERE Username = ?");
-
-          $stmt->bind_param('s', $ordersData[$i]["UsernameFornitore"]);
-
-          $stmt->execute();
-
-          $result = $stmt->get_result();
-
-          while($row = $result->fetch_assoc()){
-              $deliveryData[] = $row;
-          }
-          $stmt->close();
-        }
-      }
-
-      //var_dump($ordersData);
-      //var_dump($productsInOrdersData);
-      //var_dump($productsData);
-      //var_dump($deliveryData);
-
-      $response = array();
-      array_push($response, $ordersData, $productsInOrdersData, $productsData, $deliveryData);
-
-      $response_array['status'] = "success";
-      print json_encode($response);
-
+    }
       $mysqli->close();
 
   }
