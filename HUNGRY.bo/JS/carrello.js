f6ed16fb9b721dc0d1003164ea67982198553a65
Mattia Vincenzi <mattia.vincenzi2@studio.unibo.it>
2019-01-15 23:29:02 +0100

    Init cart

--- HUNGRY.bo/JS/carrello.js
+++ HUNGRY.bo/JS/carrello.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	computeInitPrice();
 
-	$(".prod-qnt").change(function() {
+	$("div#main-component").on("change", ".prod-qnt", function() {
 		computeDinamicallyPrice($(this));
 	});
 
@@ -14,8 +14,56 @@ $(document).ready(function() {
 				$("tfoot.footer").empty();
 				$("tfoot.footer").html('<tr><td class="dummy-column"></td><td class="align-center"><strong class="totalcart"></strong></td><td class="text-center"><a href="#" data-toggle="modal" data-target="#order-pay" class="btn btn-warning">Ordina e paga</a></td></tr>');
 			}
+
+			if ($(this).width() < 768) {
+				$("span.descrizione").attr("hidden", "true");
+			} else {
+				$("span.descrizione").removeAttr("hidden");
+			}
 			computeInitPrice();
 	}).trigger('resize');
+
+	$.post("../PHP/carrello.php?request=fornitori-in-carrello", function(suppliers) {
+		var html_code = "";
+		for(var i = 0; i < suppliers.length; i++) {
+			html_code += '<div class="container"><div class="card text-center supplier-cart"><div class="card-body"><strong>'+suppliers[0].NomeLocale+'</strong><div class="text-center"><img class="d-inline-block img-fluid rounded-circle local-icon" src="../res/'+suppliers[0].Icona+'" alt="local icon"/></div><div class="col-12"><table class="table table-hover table-condensed"><thead><tr><th id="id'+suppliers[0].Username+'" hidden>ID</th><th id="product'+suppliers[0].Username+'">Prodotto</th><th id="price'+suppliers[0].Username+'">Prezzo</th><th id="qnt'+suppliers[0].Username+'">Quantità</th><th id="remove'+suppliers[0].Username+'" hidden>Remove</th></tr></thead><tbody>';
+
+			var dataToSend = {
+				usernameFornitore: suppliers[0].Username
+			};
+
+			$.ajax({
+          url: "../PHP/carrello.php?request=prodotti-in-carrello",
+          type: "POST",
+          async: false,
+          dataType: "json",
+          data: dataToSend,
+          success: function(products) {
+						for(var i = 0; i < products.length; i++) {
+
+							dataToSend = {
+								id: products[i].IDProdotto
+							};
+
+							$.ajax({
+									url: "../PHP/carrello.php?request=informazioni-prodotto",
+									type: "POST",
+									async: false,
+									dataType: "json",
+									data: dataToSend,
+									success: function(info_product) {
+										html_code += '<tr><td headers="id'+suppliers[0].Username+'" hidden>'+info_product[0].ID+'</td><td headers="product'+suppliers[0].Username+'"><div class="row"><span>'+info_product[0].Nome+'<br/><span class="descrizione">('+products[i].Descrizione+')</span></span></div></td><td class="prod-price" headers="price'+suppliers[0].Username+'">'+info_product[0].Prezzo+'</td><td headers="qnt'+suppliers[0].Username+'"><input type="number" class="form-control text-center prod-qnt" value="'+products[i].qnta+'" min="0" max="90" name="qnt"/></td><td headers="remove'+suppliers[0].Username+'"><input class="cancel" name="cancel" type="image" src="../res/croce.png" alt="immagine croce"/></td></tr>';
+							}});
+
+						}
+      }});
+			html_code += '</tbody><tfoot class="footer"></tfoot></table></div></div></div></div>';
+		}
+
+		$("div#main-component").html(html_code);
+		window.parent.$(window.parent.document).trigger('resize');
+
+	});
 
 });
 
---
+++ HUNGRY.bo/PHP/carrello.php
@@ -0,0 +1,193 @@
+<?php
+include('functions.php');
+include("db_connect.php");
+header('Content-Type: application/json');
+
+//session
+sec_session_start();
+
+if(isset($_GET['request'])) {
+
+  //start conn
+  $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+
+  // Check connection
+  if ($mysqli->connect_error) {
+
+      $response_array['status'] = "Errore: Connessione con il DB non riuscita";
+      echo json_encode($response_array);
+      die();
+
+  }
+
+  switch ($_GET['request']) {
+    case "fornitori-in-carrello":
+      $stmt = $mysqli->prepare("SELECT IDCarrello FROM Cliente WHERE Username = ?");
+
+      if($stmt == false) {
+        $response_array['status'] = "Errore nella di ricerca dell'ID carrello";
+        print json_encode($response_array);
+        die();
+      }
+
+      $stmt->bind_param('s', $_SESSION["username"]);
+
+      $stmt->execute();
+
+      $stmt->bind_result($id_carrello);
+
+      $stmt->fetch();
+
+      $stmt->close();
+
+      $stmt = $mysqli->prepare("SELECT IDProdotto FROM ProdottoInCarrello WHERE UsernameCliente = ? AND IDCarrello = ?");
+
+      if($stmt == false) {
+        $response_array['status'] = "Errore nella query";
+        print json_encode($response_array);
+        die();
+      }
+
+      $stmt->bind_param('si', $_SESSION["username"], $id_carrello);
+
+      $stmt->execute();
+
+      $result = $stmt->get_result();
+
+      $output = array();
+      while($row = $result->fetch_assoc()){
+          $output[] = $row;
+      }
+      $stmt->close();
+
+      $suppliers = array();
+      for($x = 0; $x < count($output); $x++) {
+        $stmt = $mysqli->prepare("SELECT UsernameFornitore FROM Prodotto WHERE ID = ?");
+
+        if($stmt == false) {
+          $response_array['status'] = "Errore nella ricerca dello username associato all'idProdotto";
+          print json_encode($response_array);
+          die();
+        }
+
+        $stmt->bind_param('i', $output[$x]["IDProdotto"]);
+
+        $stmt->execute();
+
+        $stmt->bind_result($usernameFornitore);
+
+        $stmt->fetch();
+
+        $stmt->close();
+
+        if(!in_array($usernameFornitore, $suppliers)) {
+          $suppliers[] = $usernameFornitore;
+        }
+      }
+
+      $info = array();
+
+      for($x = 0; $x < count($suppliers); $x++) {
+        $stmt = $mysqli->prepare("SELECT Username, NomeLocale, Icona FROM Fornitore WHERE Username = ?");
+
+        if($stmt == false) {
+          $response_array['status'] = "Errore nella ricerca delle informazioni dei fornitori";
+          print json_encode($response_array);
+          die();
+        }
+
+        $stmt->bind_param('s', $suppliers[$x]);
+
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        while($row = $result->fetch_assoc()){
+            $info[] = $row;
+        }
+        $stmt->close();
+      }
+
+      print json_encode($info);
+
+      break;
+    case "prodotti-in-carrello":
+      if(isset($_POST["usernameFornitore"])) {
+        $stmt = $mysqli->prepare("SELECT IDCarrello FROM Cliente WHERE Username = ?");
+
+        if($stmt == false) {
+          $response_array['status'] = "Errore nella di ricerca dell'ID carrello";
+          print json_encode($response_array);
+          die();
+        }
+
+        $stmt->bind_param('s', $_SESSION["username"]);
+
+        $stmt->execute();
+
+        $stmt->bind_result($id_carrello);
+
+        $stmt->fetch();
+
+        $stmt->close();
+
+        $stmt = $mysqli->prepare("SELECT * FROM ProdottoInCarrello WHERE UsernameCliente = ? AND IDCarrello = ?");
+
+        if($stmt == false) {
+          $response_array['status'] = "Errore nella query";
+          print json_encode($response_array);
+          die();
+        }
+
+        $stmt->bind_param('si', $_SESSION["username"], $id_carrello);
+
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        $output = array();
+        while($row = $result->fetch_assoc()){
+            $output[] = $row;
+        }
+        $stmt->close();
+
+        print json_encode($output);
+      } else {
+        $response_array['status'] = "Informazioni mancanti";
+        print json_encode($response_array);
+      }
+      break;
+    case "informazioni-prodotto":
+      if(isset($_POST["id"])) {
+        $stmt = $mysqli->prepare("SELECT * FROM Prodotto WHERE ID = ?");
+
+        if($stmt == false) {
+          $response_array['status'] = "Errore nella query";
+          print json_encode($response_array);
+          die();
+        }
+
+        $stmt->bind_param('i', $_POST["id"]);
+
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        $output = array();
+        while($row = $result->fetch_assoc()){
+            $output[] = $row;
+        }
+        $stmt->close();
+
+        print json_encode($output);
+      } else {
+        $response_array['status'] = "Informazioni mancanti";
+        print json_encode($response_array);
+      }
+      break;
+  }
+
+  $mysqli->close();
+}
+
+?>
