be7471fcabbfb0ed3a07c5ed3b2aae38bf17447a
Rei Beshiri <rei.beshiri@studio.unibo.it>
2019-01-22 15:08:18 +0100

    fix admin cli icon

--- HUNGRY.bo/JS/Admin.js
+++ HUNGRY.bo/JS/Admin.js
@@ -428,6 +428,10 @@ var supplier = url.searchParams.get("supplier");
     if(refresh === "Clienti"){
       $("td[headers='modify']>span").empty();
       $("td[headers='modify']>span").html('<button type="button" class="btn btn-outline-info buttonModifycli" data-toggle="modal" data-target="#modify-from-admin-cli">Modify</button>');
+        if($(this).width() <= 981){
+          $("td[headers='modify']>span").empty();
+          $("td[headers='modify']>span").html('<a href="#" class="buttonModifycli"><img width="30px" heigth="30px" src="../res/modify-icon.png" alt="modify" data-toggle="modal" data-target="#modify-from-admin"/></a>');
+        }
       }
 
   }
